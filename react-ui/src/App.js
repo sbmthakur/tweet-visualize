0b016c4cb4cd7630a33369ddfa85fdae70256aff
shubham thakur <sbmthakur@gmail.com>
2018-01-30 21:44:25 +0530

    Set SERVER_PORT

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
  constructor(props) {
     super(props);
     console.log('wsss', process.env.REACT_APP_WEBSOCKET_PORT) 
-    let socket = io.connect('http://morning-ridge-89471.herokuapp.com:8080')// + process.env.REACT_APP_WEBSOCKET_PORT);
+    let socket = io.connect('http://morning-ridge-89471.herokuapp.com:' + process.env.REACT_APP_WEBSOCKET_PORT)// + process.env.REACT_APP_WEBSOCKET_PORT);
     this.state = {
         socket: socket,
         termFrequencies: {},
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const io = require('socket.io')(server);
 const path = require('path');
 
 //const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.SERVER_PORT;
 
 /*
  * Fetch API keys and initialize twitter client.
