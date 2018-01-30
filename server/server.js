ac65042cbf91461f5ccb35b729f7e0a78e6b18fe
shubham thakur <sbmthakur@gmail.com>
2018-01-30 18:54:16 +0530

    Serve react build

--- server/server.js
+++ server/server.js
@@ -2,8 +2,14 @@
  * Use socket.io for sending tweets to client. 
  */
 
-console.log(process.env.PORT)
-const io = require('socket.io')(process.env.PORT);
+console.log(process.env.PORT, process.env.WEBSOCKET_PORT)
+const io = require('socket.io')(process.env.WEBSOCKET_PORT);
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 /*
  * Fetch API keys and initialize twitter client.
@@ -31,4 +37,23 @@ twitterClient.stream('statuses/filter', {
   stream.on('error', function(err) {
     console.error(err);
   });
+});
+
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+// Answer API requests.
+app.get('/api', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  res.send('{"message":"Hello from the custom server!"}');
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+});
+
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
 });
